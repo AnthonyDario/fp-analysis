6b44c1c67610ab0b4ceb269cc22718129883bda2
Anthony Dario <anthonyrdario@gmail.com>
2024-02-06 16:00:13 -0800

    Add parsing specfile for eterms

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 all : test run
 
 run: 
-	dune exec --profile release -- analyzer -f foo c/test1.c -sf c/test1.spec
+	dune exec --profile release -- analyzer -f foo c/test-sbenz.c -sf c/test-sbenz.spec
+	#dune exec --profile release -- analyzer -f foo c/test1.c -sf c/test1.spec
 	#dune exec --profile release -- analyzer -f determinant c/test3.c -sf c/test3.spec
 
 test: clean
--- bin/spec.ml
+++ bin/spec.ml
@@ -11,26 +11,38 @@ open String
 open Str
 open List
 
+open Interval
 open Segment
 open Tree
 
 exception SpecFileError of string ;;
+
+let notempty s = s <> "" ;;
 
 let rec input_lines (f : in_channel) : string list =
     match input_line f with
     | Some l -> l :: input_lines f
     | None -> [] ;;
 
-let parse_aval (str : string) = 
+let parse_intr (str : string) =
     let no_brackets = global_replace (regexp "[][]") "" str in
     let split = map trim (split_on_char ';' no_brackets) in
-    AFloat (Eterm [seg_of (Float.of_string (nth split 0)) 
-                          (Float.of_string (nth split 1)) 
-                          0.0]) ;;
+    intr_of (Float.of_string (nth split 0)) (Float.of_string (nth split 1)) ;;
+
+let parse_seg (str : string) = 
+    let no_parens = global_replace (regexp "[()]") "" str in
+    let split = filter notempty (split_on_char ',' no_parens) in
+    let intr = parse_intr (nth split 0) in
+    seg_of intr.l intr.u (Float.of_string (nth split 1)) ;;
+
+let parse_eterm (str : string) = 
+    let no_braces = global_replace (regexp "[{}]") "" str in
+    let seg_strs = filter notempty (map trim (split_on_char ')' no_braces)) in
+    AFloat (Eterm (map parse_seg seg_strs))
 
 let parse_line (l : string) : (string * aval) = 
    let split = map trim (split_on_char '=' l) in
-   (nth split 0, (parse_aval (nth split 1))) ;;
+   (nth split 0, (parse_eterm (nth split 1))) ;;
 
 (* Split the file by lines *)
 (* grab each line and turn it into a value *)
